58dd7ce5382b9cc75bf41c8bc285c5ec024f0c00
tunguski <marek-romanowski@wp.pl>
2015-03-27 14:35:36 +0000

    Short version, changed panel generation method

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 (function() {
   angular.module('mtImpress', ['ngSanitize', 'btford.markdown'])
   .controller('AppCtrl', function ($scope, $http, $location) {
-    $scope.setView = function (name) {
-      $scope.view = name;
-      $location.path(name);
+    $scope.views = [
+      { title: 'Dokumentacja',          subtitle: 'Tylko tekst',
+        src: 'documentation/base' },
+      { title: 'Jakość kodu',           subtitle: 'Automatyczna walidacja',
+        src: 'code_quality/base' },
+      { title: 'Architektura',          subtitle: 'Open source',
+        src: 'architecture/base' },
+      { title: 'Środowisko testowe',    subtitle: 'Docker',
+        src: 'integration_tests/base' },
+      { title: 'Utrzymanie',            subtitle: 'Monitoring',
+        src: 'maintenance/base' },
+      { title: 'Wdrożenie produkcyjne', subtitle: 'Skalowalność, downtime',
+        src: 'production/base' },
+    ];
+    
+    $scope.setView = function (view) {
+      $scope.view = view;
+      $location.path('pages/' + view.src + '.smd');
     };
     
-    if ($location.path()) {
-      $scope.view = $location.path();
-    } else {
-      $scope.view = 'pages/intro.smd';
-    }
+    $scope.$on('$locationChangeStart', function () {
+      console.log('Location changed!');
+      $scope.viewPath = $location.path();
+    });
+    
+    $scope.setView({
+      src: $location.path() ? $location.path() : 'intro'
+    });
   });
 })();
